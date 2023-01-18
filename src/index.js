f2eea7c2f9f34cdca0275eab48153a59907f2317
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-18 21:48:55 +0100

    refactor: index. deleting: config/db

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const smurfsRouters = require('./routers/smurfs_routers');
 const smurfRouters = require('./routers/smurf_routers');
-require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT;
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const dbCO = await mongoose.connect(
+      process.env.MONGODB_CONNECTION, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      });
+    console.log(`MongoDB Connected: ${dbCO.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 app.use(express.json());
 
 app.use('/api/smurfs', smurfsRouters);
 app.use('/api/smurf', smurfRouters);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Listening for requests');
+  });
+});

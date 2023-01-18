7f05922c1f490ff19b83874dac216aa3ffb56aa0
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-18 21:36:29 +0100

    refactor: config/db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    moongoose.set('strictQuery', false);
+    mongoose.set('strictQuery', false);
     const dbCO = await mongoose.connect(
       process.env.MONGODB_CONNECTION, {
         useUnifiedTopology: true,
